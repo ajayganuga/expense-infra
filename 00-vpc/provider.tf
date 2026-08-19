c4e7eabf5334a3d8ca365f39f54ad24a6ec658bd
ajayganuga <ajayganuga@gmail.com>
2026-08-19 22:29:01 +0530

    backend

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "6.60.0"
     }
   }
 
   backend "s3" {
-    bucket = "82s-tf-remote-state-dev"
-    key    = "expense-dev-eks-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-    region = "us-east-1"
-    dynamodb_table = "82s-tf-remote-state-dev"
+    bucket       = "ajay-terraform-state-2026"
+    key          = "expense-cicd" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
 
